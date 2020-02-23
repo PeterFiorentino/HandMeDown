6a6518d4f25379ba7b3c996e829e8599718b2554
Hupaul <hupaulcamacho@pursuit.org>
2020-02-23 15:37:54 -0500

    create blank template

--- client/src/Components/History.jsx
+++ client/src/Components/History.jsx
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
 
+class History extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default History
