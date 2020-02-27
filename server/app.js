2fd8aa4a1f5be1b94dbb1ad75827746956232e2c
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-26 19:58:10 -0500

    helkp from jr part32

--- server/app.js
+++ server/app.js
@@ -19,11 +19,11 @@ const historiesRouter = require('./routes/histories');
 
 var app = express();
 
-// app.use(logger('dev'));
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(logger('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
