d975537ed5822f14a296c2008206b5596abb4347
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-26 19:51:34 -0500

    helkp from jr part 2

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ var app = express();
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: false}));
 // app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
