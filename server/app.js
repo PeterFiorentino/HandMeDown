456b0d3ce24803c8f6dfa09acd04dd52c6cc9a18
PeterFiorentino <peterfiorentino@pursuit.org>
2020-02-26 19:20:20 -0500

    changed the file location in the express static

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "client/src")));
+app.use(express.static(path.join(__dirname, "../client/src")));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
